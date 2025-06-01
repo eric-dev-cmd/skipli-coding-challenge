ba48743a043d0c47a9cb8551bbac953918a98731
Trần Trung Vinh <vinhtt@vietnamblockchain.asia>
2025-06-01 14:19:38 +0700

    refactor(server): remove debug log from phone number normalization utility

--- server/src/utils/phoneHelper.ts
+++ server/src/utils/phoneHelper.ts
@@ -2,7 +2,6 @@ import { parsePhoneNumberFromString } from "libphonenumber-js";
 
 export const normalizePhoneNumber = (input: string): string => {
   const phoneNumber = parsePhoneNumberFromString(input);
-  console.log("eric phoneNumberL ", phoneNumber);
   if (!phoneNumber || !phoneNumber.isValid()) {
     throw new Error("Invalid phone number format");
   }
