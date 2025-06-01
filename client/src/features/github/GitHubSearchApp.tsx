33c1dc3ebed99de39ec71857d7ca59a18855cdf5
Trần Trung Vinh <vinhtt@vietnamblockchain.asia>
2025-06-01 13:01:49 +0700

    refactor(client): adjust SelectTrigger width in GitHubSearchApp for better responsiveness

--- client/src/features/github/GitHubSearchApp.tsx
+++ client/src/features/github/GitHubSearchApp.tsx
@@ -204,7 +204,7 @@ export default function GitHubSearchApp() {
                 value={resultsPerPage.toString()}
                 onValueChange={handleResultsPerPageChange}
               >
-                <SelectTrigger className="w-16 sm:w-20 bg-white cursor-pointer">
+                <SelectTrigger className="w-auto bg-white cursor-pointer">
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
